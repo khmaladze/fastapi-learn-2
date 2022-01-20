8f7879bfc1fb3561375bc7832758971fe651fba0
khmaladze <khmaladze.g.7@gmail.com>
2022-01-20 23:57:21 +0400

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI,Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,19 +8,24 @@ students = {
     1:{
         "name":"john",
         "age":17,
-        "class":"Year 12"
+        "year":"Year 12"
     },
     2:{
         "name":"john 2",
-        "age":17,
-        "class":"Year 12"
+        "age":18,
+        "year":"Year 12"
     },
     3:{
         "name":"john 3",
-        "age":17,
-        "class":"Year 12"
+        "age":19,
+        "year":"Year 12"
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 @app.get('/hello')
 def hello():
@@ -29,3 +36,18 @@ def get_student(student_id: int = Path(None,description="The ID of the student y
     return students[student_id]
 
 # google.com/result?search=something
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*,student_id: int, name: Optional[str] = None, test: int, age: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            if students[student_id]["age"] == age:
+                return students[student_id]
+    return {"Data":"Not Found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error":"Student exists"}
+    students[student_id] = student
+    return students[student_id]
